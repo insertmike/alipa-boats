2978d2112b3d4990a2e4185f40377c473455ebf9
Mihail Yonchev <myonchev99@gmail.com>
2019-07-12 00:02:35 +0300

    Added Boats To Catalogue Section , Added Pictures To Gallery Section

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ function openNav() {
 function closeNav() {
     document.getElementById('mobile__menu').style.width = '0%';
 }
+
+const config = {
+    rootMargin: '0px 0px 50px 0px',
+    threshold: 0
+  };
+
+  let observer = new intersectionObserver(function(entries, self) {
+    // iterate over each entry
+    entries.forEach(entry => {
+      // process just the images that are intersecting.
+      // isIntersecting is a property exposed by the interface
+      if(entry.isIntersecting) {
+        // custom function that copies the path to the img
+        // from data-src to src
+        preloadImage(entry.target);
+        // the image is now in place, stop watching
+        self.unobserve(entry.target);
+      }
+    });
+  }, config);
+
+  const imgs = document.querySelectorAll('[data-src]');
+imgs.forEach(img => {
+  observer.observe(img);
+});
